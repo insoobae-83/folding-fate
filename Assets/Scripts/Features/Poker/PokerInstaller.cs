ea716c7646da4b72437a53c5c5ec00b9088e6b54
insoobae-83 <insoobae83@gmail.com>
2026-04-10 15:22:45 +0900

    feat(poker): add PokerInstaller VContainer LifetimeScope

---
+++ Assets/Scripts/Features/Poker/PokerInstaller.cs
@@ -0,0 +1,25 @@
+using VContainer;
+using VContainer.Unity;
+using FoldingFate.Features.Card.Systems;
+using FoldingFate.Features.Poker.Controllers;
+using FoldingFate.Features.Poker.Models;
+using FoldingFate.Features.Poker.Systems;
+using FoldingFate.Features.Poker.UI.ViewModels;
+using FoldingFate.Features.Poker.UI.Views;
+
+namespace FoldingFate.Features.Poker
+{
+    public class PokerInstaller : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<DeckModel>(Lifetime.Singleton);
+            builder.Register<HandModel>(Lifetime.Singleton);
+            builder.Register<HandEvaluator>(Lifetime.Singleton);
+            builder.Register<DealSystem>(Lifetime.Singleton);
+            builder.Register<PokerViewModel>(Lifetime.Singleton);
+            builder.RegisterEntryPoint<RoundController>();
+            builder.RegisterComponentInHierarchy<PokerView>();
+        }
+    }
+}
